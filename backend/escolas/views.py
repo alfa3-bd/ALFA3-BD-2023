aa234980b6257939d5f47fc1bd8559393122d44e
Marcoskisto <marcos@kisto.com.br>
2023-04-26 19:15:49 +0000

    implementação Node e Infraestrutura

--- backend/escolas/views.py
+++ backend/escolas/views.py
@@ -1,24 +1,31 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from escolas.models import Contrato, UnidadeEscolar
-from escolas.serializers import (
-    ContratoSerializer, UnidadeEscolarSerializer, UserSerializer
-)
+from escolas import models
+from escolas import serializers
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = serializers.UserSerializer
 
 
 class UnidadeEscolar(viewsets.ModelViewSet):
-    queryset = UnidadeEscolar.objects.all()
-    serializer_class = UnidadeEscolarSerializer
+    queryset = models.UnidadeEscolar.objects.all()
+    serializer_class = serializers.UnidadeEscolarSerializer
 
 
 class Contrato(viewsets.ModelViewSet):
-    queryset = Contrato.objects.all()
-    serializer_class = ContratoSerializer
+    queryset = models.Contrato.objects.all()
+    serializer_class = serializers.ContratoSerializer
+
+class Node(viewsets.ModelViewSet):
+    queryset = models.Node.objects.all()
+    serializer_class = serializers.NodeSerializer
+
+class Infraestrutura(viewsets.ModelViewSet):
+    queryset = models.Infraestrutura.objects.all()
+    serializer_class = serializers.NodeSerializer
 
 # class Relatorio(viewsets)
